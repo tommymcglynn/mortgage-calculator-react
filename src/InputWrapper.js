eb60707abbee8c7f1c12b7d9894e8aa171de0e5f
Yash Karanke <dex.papa@gmail.com>
2018-12-14 23:35:46 +0530

    Updating var
    + It has been noticed that Safari  > 9.5 does not support let keyword even after using polyfills, so further commits will be for this.

--- src/InputWrapper.js
+++ src/InputWrapper.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class InputWrapper extends React.Component {
     render() {
         const {label, styles, children, subtext} = this.props;
-        let i = 0;
+        var i = 0;
         const inputs = React.Children.toArray(children).map((child) =>
             <div className={styles.inputSection} key={i++}>
                 {child}
