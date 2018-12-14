c50db296ff542f1f3a4f8806f5fa19164544703d
Yash Karanke <dex.papa@gmail.com>
2018-12-15 00:29:54 +0530

    Legacy browsers
    + Replaced `let` with `var` as it did not supported legacy browsers such as Safari > 9.5

--- src/Switch.js
+++ src/Switch.js
@@ -1,20 +1,21 @@
-var React = require('react');
-import Styles from './Switch.css';
-
+var React = require("react");
+import Styles from "./Switch.css";
 
 export default class Switch extends React.Component {
-    render() {
-        const styles = Styles;
-        const {active} = this.props;
-        let wrapperClass = styles.switchWrapper;
-        if (active) {
-            wrapperClass += " "+styles.switchWrapperActive;
-        }
-        return (
-            <div className={wrapperClass} onClick={() => this.props.onChange(!active)}>
-                <div className={styles.switchBubble}>
-                </div>
-            </div>
-        );
+  render() {
+    const styles = Styles;
+    const { active } = this.props;
+    var wrapperClass = styles.switchWrapper;
+    if (active) {
+      wrapperClass += " " + styles.switchWrapperActive;
     }
+    return (
+      <div
+        className={wrapperClass}
+        onClick={() => this.props.onChange(!active)}
+      >
+        <div className={styles.switchBubble} />
+      </div>
+    );
+  }
 }
--- src/Util.js
+++ src/Util.js
@@ -1,67 +1,71 @@
-const moneyFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0
 });
 
-const penniesFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+const penniesFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
 });
 
-const percentFormatter = new Intl.NumberFormat('en-US', {
-    style: 'percent',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2,
+const percentFormatter = new Intl.NumberFormat("en-US", {
+  style: "percent",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 2
 });
 
 export default class Util {
-    static percentToValue(percent) {
-        let value = parseFloat(percent);
-        if (isNaN(value)) {
-            return NaN;
-        }
-        if (value < 0) {
-            return 0;
-        }
-        return value / 100;
+  static percentToValue(percent) {
+    var value = parseFloat(percent);
+    if (isNaN(value)) {
+      return NaN;
     }
+    if (value < 0) {
+      return 0;
+    }
+    return value / 100;
+  }
 
-    static moneyToValue(money) {
-        if (money === undefined) return '';
-        let value = parseInt(money.replace(/\D/g, ""));
-        return !isNaN(value) ? value : '';
-    }
+  static moneyToValue(money) {
+    if (money === undefined) return "";
+    var value = parseInt(money.replace(/\D/g, ""));
+    return !isNaN(value) ? value : "";
+  }
 
-    static moneyValue(amount, showPennies = false, withSymbol = true) {
-        if (amount === null || amount === '') return '';
-        let value = showPennies ? penniesFormatter.format(amount) : moneyFormatter.format(amount);
-        if (withSymbol === false) {
-            return value.substring(1);
-        }
-        return value;
+  static moneyValue(amount, showPennies = false, withSymbol = true) {
+    if (amount === null || amount === "") return "";
+    var value = showPennies
+      ? penniesFormatter.format(amount)
+      : moneyFormatter.format(amount);
+    if (withSymbol === false) {
+      return value.substring(1);
     }
+    return value;
+  }
 
-    static percentValue(amount, withSymbol) {
-        if (amount === null || amount === '') return '';
-        let value = percentFormatter.format(amount);
-        if (withSymbol === false) {
-            return value.substring(0, value.length - 1);
-        }
-        return value;
+  static percentValue(amount, withSymbol) {
+    if (amount === null || amount === "") return "";
+    var value = percentFormatter.format(amount);
+    if (withSymbol === false) {
+      return value.substring(0, value.length - 1);
     }
+    return value;
+  }
 
-    static numberValueOrDefault(value, minValue, defaultValue) {
-        if (value == null || isNaN(value) || value < minValue) return defaultValue;
-        return value;
-    }
+  static numberValueOrDefault(value, minValue, defaultValue) {
+    if (value == null || isNaN(value) || value < minValue) return defaultValue;
+    return value;
+  }
 
-    static numberValueInSetOrDefault(value, possibleValues, defaultValue) {
-        if (!Array.isArray(possibleValues)) throw "possibleValues must be an array.";
-        if (value == null || isNaN(value) || !possibleValues.includes(value)) return defaultValue;
-        return value;
-    }
+  static numberValueInSetOrDefault(value, possibleValues, defaultValue) {
+    if (!Array.isArray(possibleValues))
+      throw "possibleValues must be an array.";
+    if (value == null || isNaN(value) || !possibleValues.includes(value))
+      return defaultValue;
+    return value;
+  }
 }
